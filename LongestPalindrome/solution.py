76d3825fe665a0783070197ef551a2d3edae1915
Enkai <enkaihuang2021@gmail.com>
2023-03-14 16:20:44 +0800

    Longest Palindrome

--- LongestPalindrome/solution.py
+++ LongestPalindrome/solution.py
@@ -21,3 +21,57 @@ class Solution:
 
         return total + maxodd
 
+# Build-in Function Version
+# from collections import defaultdict
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+#         letters = defaultdict(int)
+
+#         if len(s) == 1:
+#             return 1
+
+#         for char in s:
+#             letters[char] += 1
+
+#         total = 0
+#         maxodd = 0
+
+#         for b in letters.values():
+#             if b % 2 == 0:
+#                 total += b
+#             else:
+#                 total += (b - 1)
+#                 maxodd = 1
+
+#         return total + maxodd
+
+
+
+
+
+# Simplified Version
+# from collections import defaultdict
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+#         letters = defaultdict(int)
+
+#         if len(s) == 1:
+#             return 1
+
+#         for char in s:
+#             letters[char] += 1
+
+#         total = 0
+#         maxodd = 0
+
+#         for b in letters.values():
+#             if b % 2 == 0:
+#                 total += b
+#             else:
+#                 total += (b - 1)
+#                 maxodd = 1
+
+#         return total + maxodd if maxodd else total
+
